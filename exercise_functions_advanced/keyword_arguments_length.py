db41048028a3b7e3a69c54cbed3f5a838f5a149e
gyurel <gyurel@yahoo.com>
2021-10-05 00:30:06 +0200

    Problem 7

---
+++ exercise_functions_advanced/keyword_arguments_length.py
@@ -0,0 +1,13 @@
+def kwargs_length(**kwargs):
+    result = len(kwargs)
+    return result
+
+
+# dictionary = {'name': 'Peter', 'age': 25}
+#
+# print(kwargs_length(**dictionary))
+
+
+dictionary = {}
+
+print(kwargs_length(**dictionary))
