e45bb600e181ff17b317c3b6ddfd307e4abae4ba
gyurel <gyurel@yahoo.com>
2021-10-09 23:22:52 +0200

    Problem 2

--- exercises_multidimensional_lists/diagonal_difference.py
+++ exercises_multidimensional_lists/diagonal_difference.py
@@ -1,21 +1,21 @@
-import sys
-from io import StringIO
-
-test_input1 = """3
-11 2 4
-4 5 6
-10 8 -12
-"""
-
-test_input2 = """4
--7 14 9 -20
-3 4 9 21
--14 6 8 44
-30 9 7 -14
-"""
-
+# import sys
+# from io import StringIO
+#
+# test_input1 = """3
+# 11 2 4
+# 4 5 6
+# 10 8 -12
+# """
+#
+# test_input2 = """4
+# -7 14 9 -20
+# 3 4 9 21
+# -14 6 8 44
+# 30 9 7 -14
+# """
+#
 # sys.stdin = StringIO(test_input1)
-sys.stdin = StringIO(test_input2)
+# sys.stdin = StringIO(test_input2)
 
 n = int(input())
 
